8ae51b21a1061c68029e5c198b4d1552ad87df81
josue95 <cuvala95_illuek@hotmail.com>
2016-06-01 19:29:08 +0200

    Version 4. Hemos terminado la clase Nomina y la clase TodosEmpleados terminando asi casi nuestro proyecto.

--- src/proyectoprogramacion/Nomina.java
+++ src/proyectoprogramacion/Nomina.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoprogramacion;
 
-/**
- *
- * @author Usuario
- */
+
 public class Nomina {
+    static final double salario_minimo=600, ayuda=100;
     
+    static final float irpf_max=24f, irpf_min=9f;
+    
+     public static void muestraNomina(String nombre, int nHijos, double plus) {
+        double extra=0;
+        double Hijos=0;
+        float irpf;
+        double irpfFinal;
+        
+         System.out.printf("%40s", "NOMINA\n");
+         System.out.print("Fecha: ");
+         Pantalla.miFecha.muestraFecha();
+         System.out.println("");
+         
+         System.out.println("Empleado: " + nombre + " Numero de hijos: " + nHijos);
+         System.out.println("\t" + Empleado.getSalarioBase());
+         
+         System.out.printf("%40s", "Salario Base: ");
+         System.out.println("\t" + Empleado.getSalarioBase());
+         
+         System.out.printf("%40s", "Plus");
+         System.out.println("\t" + plus);
+         
+         if (Pantalla.miFecha.mes==6 || Pantalla.miFecha.mes==12){
+             System.out.printf("%40s", "Extraordinaria: ");
+             extra=Empleado.getSalarioBase();
+             System.out.println("\t" + extra);
+         }
+         
+         if (Pantalla.miFecha.mes==9) {
+             System.out.printf("%40s", "Ayuda por hijos: ");
+             Hijos=ayuda*nHijos;
+             System.out.println("\t" + Hijos);
+         }
+         
+         if ((Empleado.getSalarioBase()+plus+extra+Hijos)<salario_minimo) {
+             irpf=irpf_min;
+            }else{irpf=irpf_max;}
+            irpf=(irpf-nHijos);
+            if (irpf<1){
+                irpf=1;
+         }
+         
+         System.out.printf("%40s", irpf + "% de IRPF: ");
+         irpfFinal=((Empleado.getSalarioBase()+plus+extra+Hijos)/100)*irpf;
+         System.out.println("\t" + irpfFinal);
+         System.out.println("\t\t\t----------------------------------");
+         System.out.printf("%40s", "Total a recibir: ");
+         System.out.println("\t" + (Empleado.getSalarioBase()+plus+extra+Hijos-irpf));
+         System.out.println("");
+    }    
 }
+
+
+
+
--- src/proyectoprogramacion/TodosEmpleados.java
+++ src/proyectoprogramacion/TodosEmpleados.java
@@ -10,5 +10,22 @@ package proyectoprogramacion;
  * @author Usuario
  */
 public class TodosEmpleados {
+     static Empleado e1, e2, e3, e4, e5, e6, e7, e8, e9, e10;
     
+    static void altaEmpleado() {
+        e1= new Empleado("Em Uno", 100,1);
+        e2= new Empleado("Em Dos", 200,2);
+        e3= new Empleado("Em Tres", 300,3);
+        e4= new Empleado("Em Cuatro", 400,4);
+        e5= new Empleado("Em Cinco", 500,5);
+        e6= new Empleado("Em Seis", 600,6);
+        e7= new Empleado("Em Siete", 700,7);
+        e8= new Empleado("Em Ocho", 800,8);
+        e9= new Empleado("Em Nueve", 900,9);
+        e10= new Empleado(Pantalla.pideCadena("Inttroduce el nombre del empleado: "),
+            Pantalla.pideDouble("Introduce el plus de salario: "),
+            Pantalla.pideEntero("Introduce en nº de hijos: "));                
+    }
 }
+
+
