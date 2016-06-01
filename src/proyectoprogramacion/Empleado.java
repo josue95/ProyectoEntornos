8da8e4ee9ebf6836ab1de1bd2bf3ae780b6f280a
josue95 <cuvala95_illuek@hotmail.com>
2016-06-01 19:22:48 +0200

    Version 2. Hemos terminado la clase Empleado y la clase Numero aun esta incompleto en proyecto.

--- src/proyectoprogramacion/Empleado.java
+++ src/proyectoprogramacion/Empleado.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoprogramacion;
 
-/**
- *
- * @author Usuario
- */
+
 public class Empleado {
+     private String nombre;
+    private double plus;
+    private int nHijos;
+    private static double salarioBase;
+
+    Empleado(String nombre, double plus, int hijos) {
+        this.nombre = nombre;
+        this.plus = plus;
+        this.nHijos = hijos;
+    }
+
+    /**
+     * @param asalarioBase
+     */
+   public static void setSalarioBase(double asalarioBase) {
+        salarioBase = asalarioBase;
+        
+    }
+    public static double getSalarioBase() {
+        return salarioBase;
+    }
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the plus
+     */
+    public double getPlus() {
+        return plus;
+    }
+
+    /**
+     * @param plus the plus to set
+     */
+    public void setPlus(double plus) {
+        this.plus = plus;
+    }
+
+    /**
+     * @return the nHijos
+     */
+    public int getnHijos() {
+        return nHijos;
+    }
+
+    /**
+     * @param nHijos the nHijos to set
+     */
+    public void setnHijos(int nHijos) {
+        this.nHijos = nHijos;
+    }
     
+
+}
+
 }
--- src/proyectoprogramacion/Numero.java
+++ src/proyectoprogramacion/Numero.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoprogramacion;
 
-/**
- *
- * @author Usuario
- */
+
 public class Numero {
-    
+    int num;
+  
+  boolean correcto() {
+      return (num >=0 && num <=99);
+  }
+  
+  String dameNumero(int n) {
+      switch(n) {
+          case 0:
+              return "cero";
+          case 1:
+              return "uno";
+          case 2:
+              return "dos";
+          case 3:
+              return "tres";
+          case 4:
+              return "cuatro";
+          case 5:
+              return "cinco";
+          case 6:
+              return "seis";
+          case 7:
+              return "siete";
+          case 8:
+              return "ocho";
+          case 9:
+              return "nueve";
+          case 10:
+              return "diez";
+          case 11:
+              return "once";
+          case 12:
+              return "doce";
+          case 13:
+              return "trece";
+          case 14:
+              return "catorce";
+          case 15:
+              return "quince";
+          case 30:
+              return "treinta";
+          case 40:
+              return "cuarenta";
+          case 50:
+              return "cincuenta";
+          case 60:
+              return "sesenta";
+          case 70:
+              return "setenta";
+          case 80:
+              return "ochenta";
+          case 90:
+              return "noventa";
+          default:
+            return "";
+    }
+  }      
+
+  String numeroCompuesto(int num) {
+      String cad;
+      int decenas, unidades;
+      decenas  = num / 10;
+      unidades = num % 10;
+      if (decenas > 2) {
+          cad = dameNumero(decenas*10) + " y ";
+      }else {
+          if (decenas == 1) {
+              cad="dieci";
+          }else {
+              cad="veinti";
+            }
+        } 
+  cad +=(unidades);
+  return cad;
+  }
+  
+  void muestraFin(int n) {
+      System.out.println("\n\t\tEl Numero " + n + "Es: " + dameNumero(n));
+  }
+}
+
+
 }
